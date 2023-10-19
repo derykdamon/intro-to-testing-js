41717b5633a3b8b2ac498c8ab7239e3ba99dd09a
Deryk Damon <derykdamon@aol.com>
2023-10-18 23:21:35 -0500

    added the first test and solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -13,3 +13,20 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+
+// Unit tests for the sayHello function
+describe('helloYou', function() {
+    it('should be a defined function', function() {
+        expect(typeof helloYou).toBe('function');
+    });
+    it('should return a string when called', function() {
+        expect(typeof helloYou()).toBe("string");
+    });
+    it('should return the string "Hello, You!" when executed', function() {
+        expect(helloYou()).toBe("Hello, You!");
+    });
+    it("should never return 'undefined' when called", function() {
+        expect(helloYou()).not.toBe(undefined);
+    });
+});
